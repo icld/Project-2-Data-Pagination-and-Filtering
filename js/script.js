9235a5939a0aa72ad1863f7c03ffca7368c5118d
icld <76025734+icld@users.noreply.github.com>
2021-02-24 12:54:54 -0800

    added search

--- js/script.js
+++ js/script.js
@@ -1,43 +1,29 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
+
+const header = document.querySelector('.header')
+header.insertAdjacentHTML('beforeend', `<form class='form' type= 'submit'><label for="search" class="student-search">
+<input id="search-input" placeholder="Search by name...">
+<button id="sub" type="submit"><img src="img/icn-search.svg" alt="Search icon"></button>
+</label></form>`)
+
+const itemsPerPage = 9
+const linkList = document.querySelector(".link-list")
+const studentList = document.querySelector('.student-list')
+const pageDiv = document.querySelector(".page")
+const form = document.querySelector('.form');
 
 
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
 function showPage(list, page) {
-   // create two variables which will represent the index for the first and last student on the page
-   const startIndex = ((page * 9) - 9)
-   const endIndex = page * 9
+   const startIndex = (page * itemsPerPage) - itemsPerPage
+   const endIndex = page * itemsPerPage
 
-   console.log(startIndex)
-   console.log(endIndex)
-   // select the element with a class of `student-list` and assign it to a variable
-
-   const studentList = document.querySelector('.student-list')
-   // set the innerHTML property of the variable you just created to an empty string
    studentList.innerHTML = '';
-   // loop over the length of the `list` parameter
-   for (let i = 0; i < list.length; i++) {
-
+   for (let i in list) {
       if (i >= startIndex && i < endIndex) {
-         const stu = list[i]
-         const name = `${stu.name.title}. ${stu.name.first} ${stu.name.last}`;
-         const email = `${stu.email}`
-         const img = `${stu.picture.medium}`
-         const reg = `${stu.registered.date}`
+         const name = `${list[i].name.title}. ${list[i].name.first} ${list[i].name.last}`;
+         const email = `${list[i].email}`
+         const img = `${list[i].picture.medium}`
+         const reg = `${list[i].registered.date}`
          const studentItem = `<li class="student-item cf">
          <div class="student-details">
            <img class="avatar" src=${img} alt="Profile Picture">
@@ -47,44 +33,94 @@ function showPage(list, page) {
          <div class="joined-details">
            <span class="date">${reg}</span>
          </div>
-       </li>`
+       </li>`;
          studentList.insertAdjacentHTML("beforeend", studentItem)
       }
    }
-   // inside the loop create a conditional to display the proper students
-   // inside the conditional:
-   // create the elements needed to display the student information
-   // insert the above elements
 }
+
 function addPagination(list) {
-   // create a variable to calculate the number of pages needed
-
-   // select the element with a class of `link-list` and assign it to a variable
-
-   // set the innerHTML property of the variable you just created to an empty string
-
-   // loop over the number of pages needed
-   // create the elements needed to display the pagination button
-   // insert the above elements
-
-   // give the first pagination button a class of "active"
-
-   // create an event listener on the `link-list` element
-   // if the click target is a button:
-   // remove the "active" class from the previous button
-   // add the active class to the clicked button
-   // call the showPage function passing the `list` parameter and page to display as arguments
+   const numOfPages = Math.ceil(list.length / itemsPerPage)
+   linkList.innerHTML = '';
+   for (let i = 1; i < numOfPages; i++) {
+      const addButton = `<li>
+<button id = "pagButton" type="button">${i}</button>
+</li>`;
+      linkList.insertAdjacentHTML("beforeend", addButton);
+   }
+   const firstButton = document.querySelector('#pagButton');
+   firstButton.className = 'active'
 }
 
 
+linkList.addEventListener('click', (event) => {
+   const targ = event.target
+   const buttons = document.getElementsByTagName('button')
+
+   for (let i = 0; i < buttons.length; i++) {
+      buttons[i].className = '';
+      targ.className = 'active'
+      showPage(data, targ.textContent)
+   }
+})
+
+const searchButton = document.querySelector('#sub')
+const searchInput = document.querySelector('#search-input');
+
+function alertMe() {
+   const alertMe = document.createElement('div')
+   const header = form.parentNode
+   searchButton.style.backgroundColor = 'red';
+   alertMe.className = 'alert'
+   alertMe.style.backgroundColor = 'red'
+   alertMe.style.fontSize = '2em'
+   alertMe.textContent = `Please try again`;
+   header.insertBefore(alertMe, form)
+}
+function removeAlert() {
+   searchButton.style = ''
+   const alertMe = document.getElementsByClassName('alert')[0]
+   if (alertMe) {
+      header.removeChild(alertMe)
+   }
+}
+
+function studentSearch() {
+   const userInput = searchInput.value.toLowerCase();
+   const filter = [];
+   if (userInput.length === 0 && searchButton.backgroundColor != 'red') {
+      alertMe()
+   } else if (userInput.length !== 0) {
+      removeAlert()
+      for (let i in data) {
+         const firstName = data[i].name.first.toLowerCase()
+         const lastName = data[i].name.last.toLowerCase()
+         if (firstName.includes(userInput) || lastName.includes(userInput)) {
+            filter.push(data[i]);
+         }
+
+      }
+      showPage(filter, 1)
+      addPagination(filter)
+   } else {
+      showPage(data, 1);
+      addPagination(data)
+   }
+}
+searchInput.addEventListener('keyup', () => {
+   studentSearch();
+})
+
+searchButton.addEventListener('click', (e) => {
+   e.preventDefault();
+   removeAlert()
+   studentSearch();
+})
 
 
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
 
 
 
-// Call functions
+showPage(data, 1)
+addPagination(data)
